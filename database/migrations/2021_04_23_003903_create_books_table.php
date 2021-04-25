42608abb4ecc9ef84bbad2f3f1be9b4ffa06e994
Muhammad Fairuz <fairuzzumar@gmail.com>
2021-04-25 11:00:09 +0700

    database migration

---
+++ database/migrations/2021_04_23_003903_create_books_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->unique();
+            $table->longtext('synopsis');
+            $table->enum('category',['pengetahuan', 'hiburan', 'seni']);
+            $table->year('year');
+            $table->foreignId('author_id')
+                ->reference('id')
+                ->on('authors')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
